95b47fcda2ff899b70d1965f9a45cc3d16d16ca0
ThiagoCF05 <thiago.castro.ferreira@gmail.com>
2020-11-10 10:01:00 -0300

    Rollback requirements and chrF

--- metrics/chrF.py
+++ metrics/chrF.py
@@ -125,9 +125,7 @@ def computeChrF(fpRef, fpHyp, nworder, ncorder, beta, sentence_level_scores = No
         nsent += 1
         
         # preparation for multiple references
-        maxF = 0.0
-        bestWordMatchingCount = None
-        bestCharMatchingCount = None
+        maxF = -1
         
         hypNgramCounts = ngram_counts(separate_punctuation(hline), nworder)
         hypChrNgramCounts = ngram_counts(separate_characters(hline), ncorder)
